f94d4ea275b0fded0a9eaaee2677fec54ae8e3ff
SuryaaAulia <suryaaulia489@gmail.com>
2023-03-20 07:14:09 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- MOD5/ConsoleApp1/Program.cs
+++ MOD5/ConsoleApp1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+
+Penjumlahan p = new Penjumlahan();
+p.JumlahTigaAngka<int>(13, 02, 21);
