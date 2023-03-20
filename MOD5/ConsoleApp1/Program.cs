88a972e5a44ccf78da690c50326795b0d14f7171
SuryaaAulia <suryaaulia489@gmail.com>
2023-03-20 07:43:44 +0700

    menambahkan class SimpleDataBase

--- MOD5/ConsoleApp1/Program.cs
+++ MOD5/ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+
+Penjumlahan p = new Penjumlahan();
+p.JumlahTigaAngka<int>(13, 02, 21);
+
+SimpleDataBase<String> sd = new SimpleDataBase<String>();
+sd.AddNewData("13");
+sd.AddNewData("02");
+sd.AddNewData("21");
+sd.PrintAllData();
---
+++ MOD5/ConsoleApp1/SimpleDataBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+        public void AddNewData(T t)
+        {
+            storedData.Add(t);
+            inputDates.Add(DateTime.Now);
+        }
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + (i + 1) + " berisi: " + storedData[i] + ", yang disimpan pada waktu UTC: " + inputDates[i]);
+            }
+        }
+
+    }
+}
